0b5d5698103abccb69d65d8a90168e8ae7a49014
Catalin Radoiu <radoiu.catalin96@gmail.com>
2018-05-29 20:06:52 +0200

    Add status and id to the medical appointment class
    Remove unused images

--- app/src/main/java/com/catalin/mymedic/data/MedicalAppointment.kt
+++ app/src/main/java/com/catalin/mymedic/data/MedicalAppointment.kt
@@ -4,11 +4,22 @@ package com.catalin.mymedic.data
  * @author catalinradoiu
  * @since 5/29/2018
  */
-data class MedicalAppointment(val dateTime: Long, val patientId: String, val medicId: String, val description: String) {
+data class MedicalAppointment(
+    val id: String,
+    val dateTime: Long,
+    val patientId: String,
+    val medicId: String,
+    val description: String,
+    val status: AppointmentStatus
+) {
 
     /*
         Empty constructor needed for parsing the firebase json to an actual MedicalAppointment object
         DataSnapshot.getValue(MedicalAppointment::class.java)
      */
-    constructor() : this(0, "", "", "")
+    constructor() : this("", 0, "", "", "", AppointmentStatus.AWAITING)
+}
+
+enum class AppointmentStatus {
+    CONFIRMED, AWAITING, REJECTED
 }
